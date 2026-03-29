70fc63e2396fb825f9c7568a3d09656b16d60f95
Mohammed Musab Khan <khanmusab51@gmail.com>
2026-03-29 18:56:47 +0530

    v0.1 Personal Blog Hero Section Done

---
+++ components/hero.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from "next/image"
+
+const Hero = () => {
+    return (
+        <div className="grid grid-rows-2 gap-30">
+            <div className="relative flex flex-col items-center">
+                <Image src={"/assets/images/hero-background.jpg"} alt="Background Image" className="w-full h-72 object-cover" width={700} height={100} />
+
+                <Image src={"/assets/images/hero-profile.jpg"} alt="Background Image" className="absolute bottom-0 translate-y-1/2 rounded-full border-8 border-amber-300 w-50 h-50" width={600} height={100} />
+            </div>
+            <div className="p-3">
+                <p className="m-auto">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad, et repellendus rem nobis blanditiis, mollitia, corrupti doloremque quam dicta laboriosam natus corporis dolore dolorem vitae. Modi blanditiis nulla maxime quidem!
+                    Veritatis doloribus saepe culpa corporis tempore? Cumque voluptas illo quia veritatis doloribus repellat iste nisi accusantium pariatur eum aspernatur quaerat provident non, nesciunt temporibus excepturi quisquam qui nostrum id ut!
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Hero
